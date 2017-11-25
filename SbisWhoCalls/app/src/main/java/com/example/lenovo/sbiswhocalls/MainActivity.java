2a8876f459bd3fd1597592026c66d759aa26919f
slon <serafim.gerasimov@mail.ru>
2017-11-25 15:43:22 +0300

    init

--- SbisWhoCalls/app/src/main/java/com/example/lenovo/sbiswhocalls/MainActivity.java
+++ SbisWhoCalls/app/src/main/java/com/example/lenovo/sbiswhocalls/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lenovo.sbiswhocalls;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }

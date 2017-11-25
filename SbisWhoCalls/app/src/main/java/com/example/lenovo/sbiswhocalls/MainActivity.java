5b70cee253fd82864e91c95d6bd0b0cae0444aad
slon <serafim.gerasimov@mail.ru>
2017-11-25 20:49:02 +0300

    тестовы аларм(не рабочий)

--- SbisWhoCalls/app/src/main/java/com/example/lenovo/sbiswhocalls/MainActivity.java
+++ SbisWhoCalls/app/src/main/java/com/example/lenovo/sbiswhocalls/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.lenovo.sbiswhocalls;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v){
+        Intent intent = new Intent(this, info_activity.class);
+        startActivity(intent);
     }
 
 
